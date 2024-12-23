153be32768a3713bc5ed328f962023d57f24209a
EasyMinute <nikyura@gmail.com>
2024-12-23 17:40:21 +0200

    acf

--- wp-content/themes/proacto-theme/functions-parts/acf-settings.php
+++ wp-content/themes/proacto-theme/functions-parts/acf-settings.php
@@ -268,3 +268,168 @@ if ( class_exists( 'Acf_Config' ) ) {
 
     new Acf_Config();
 }
+
+
+
+add_action( 'acf/include_fields', function() {
+	if ( ! function_exists( 'acf_add_local_field_group' ) ) {
+		return;
+	}
+
+	acf_add_local_field_group( array(
+		'key' => 'group_66f4085bd2cc1',
+		'title' => 'Post options',
+		'fields' => array(
+			array(
+				'key' => 'field_66f4085ce0ffe',
+				'label' => 'Post options',
+				'name' => 'post_options',
+				'aria-label' => '',
+				'type' => 'group',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'layout' => 'block',
+				'sub_fields' => array(
+					array(
+						'key' => 'field_66f40886e0fff',
+						'label' => 'Author',
+						'name' => 'author',
+						'aria-label' => '',
+						'type' => 'group',
+						'instructions' => '',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array(
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'layout' => 'table',
+						'sub_fields' => array(
+							array(
+								'key' => 'field_66f40a49e1000',
+								'label' => 'Name',
+								'name' => 'name',
+								'aria-label' => '',
+								'type' => 'text',
+								'instructions' => '',
+								'required' => 0,
+								'conditional_logic' => 0,
+								'wrapper' => array(
+									'width' => '',
+									'class' => '',
+									'id' => '',
+								),
+								'default_value' => '',
+								'maxlength' => '',
+								'placeholder' => '',
+								'prepend' => '',
+								'append' => '',
+							),
+							array(
+								'key' => 'field_66f40a4fe1001',
+								'label' => 'Photo',
+								'name' => 'photo',
+								'aria-label' => '',
+								'type' => 'image',
+								'instructions' => '',
+								'required' => 0,
+								'conditional_logic' => 0,
+								'wrapper' => array(
+									'width' => '',
+									'class' => '',
+									'id' => '',
+								),
+								'return_format' => 'array',
+								'library' => 'all',
+								'min_width' => '',
+								'min_height' => '',
+								'min_size' => '',
+								'max_width' => '',
+								'max_height' => '',
+								'max_size' => '',
+								'mime_types' => '',
+								'preview_size' => 'thumbnail',
+							),
+						),
+					),
+					array(
+						'key' => 'field_66f40a62e1002',
+						'label' => 'Baner desktop',
+						'name' => 'baner_desktop',
+						'aria-label' => '',
+						'type' => 'image',
+						'instructions' => '1440x420',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array(
+							'width' => '50',
+							'class' => '',
+							'id' => '',
+						),
+						'return_format' => 'array',
+						'library' => 'all',
+						'min_width' => '',
+						'min_height' => '',
+						'min_size' => '',
+						'max_width' => '',
+						'max_height' => '',
+						'max_size' => '',
+						'mime_types' => '',
+						'preview_size' => 'medium_large',
+					),
+					array(
+						'key' => 'field_66f40abde1003',
+						'label' => 'Baner mobile',
+						'name' => 'baner_mobile',
+						'aria-label' => '',
+						'type' => 'image',
+						'instructions' => '360x579',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array(
+							'width' => '50',
+							'class' => '',
+							'id' => '',
+						),
+						'return_format' => 'array',
+						'library' => 'all',
+						'min_width' => '',
+						'min_height' => '',
+						'min_size' => '',
+						'max_width' => '',
+						'max_height' => '',
+						'max_size' => '',
+						'mime_types' => '',
+						'preview_size' => 'medium_large',
+					),
+				),
+			),
+		),
+		'location' => array(
+			array(
+				array(
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'post',
+				),
+			),
+		),
+		'menu_order' => 0,
+		'position' => 'normal',
+		'style' => 'default',
+		'label_placement' => 'top',
+		'instruction_placement' => 'label',
+		'hide_on_screen' => '',
+		'active' => true,
+		'description' => '',
+		'show_in_rest' => 0,
+	) );
+} );
+
